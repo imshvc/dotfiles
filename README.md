764f3cce78895ad15a718521885ffa0468029bba
Nurudin Imsirovic <realnurudinimsirovic@gmail.com>
2025-01-24 05:54:17 +0100

    push: updates revision 3

--- README.md
+++ README.md
@@ -1,9 +1,65 @@
 # imshvc's dotfiles
 
-One-liner setup:
+Personal dotfiles with many goodies.
 
-```text
+## Warnings
+
+**WARNING**: Overwrites any existing files.
+
+Bootstrap will not proceed if the file exists: `~/.dotfiles_bootstrapped`
+
+You'll have to manually remove the file:
+
+```bash
+rm -f ~/.dotfiles_bootstrapped
+```
+
+## Bootstrap: regular user
+
+Remote:
+
+```bash
 curl -sSL https://imshvc.github.io/dotfiles | /bin/bash -i 2>/dev/null
+```
+
+Local (assumes you've cloned the repo):
+
+```bash
+./scripts/bootstrap-local.sh 2>/dev/null
+```
+
+## Bootstrap: root user
+
+### Method 1 (source files from other user)
+
+Sourcing files from other user's home folder is not recommended
+for security reasons, but I structured my files so that they play
+nicely alongside two users (the regular user, and root user).
+
+But then again - you should be bootstrapping into the root folder
+separately, see [Method 2 (bootstrap root user)](#method-2-bootstrap-root-user).
+
+**Run these commands as a regular user:**
+
+```bash
+sudo cp /home/$USER/.bashrc /root/.bashrc
+sudo sed "s|\$HOME|/home/$USER|g" /root/.bashrc | sudo tee -a /root/.bashrc >&/dev/null
+```
+
+### Method 2 (bootstrap root user)
+
+**Run these commands as a root user:**
+
+Remote:
+
+```bash
+curl -sSL https://imshvc.github.io/dotfiles | ALLOW_ROOT=1 /bin/bash -i 2>/dev/null
+```
+
+Local (assumes you've cloned the repo):
+
+```bash
+ALLOW_ROOT=1 ./scripts/bootstrap-local.sh 2>/dev/null
 ```
 
 ## Documentation
---
+++ scripts/bootstrap-local.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+# authors:    Nurudin Imsirovic <imshvc>
+# file:       bootstrap-local.sh
+# desc:       bootstrap my dotfiles locally
+# created:    2025-01-24 05:26 AM
+# updated:    2025-01-24 05:40 AM
+# repository: https://github.com/imshvc/dotfiles
+
+# one-liner: ./scripts/bootstrap-local.sh 2>/dev/null
+
+# root user: 2 methods
+# -- method 1: source files from other user
+#      run these commands as a regular user:
+#        1. sudo cp /home/$USER/.bashrc /root/.bashrc
+#        2. sudo sed "s|\$HOME|/home/$USER|g" /root/.bashrc | sudo tee -a /root/.bashrc >&/dev/null
+#
+#      sourcing files from other user's home folder is not recommended
+#      for security reasons, but I structured my files so that they play
+#      nicely alongside two users (the regular user, and root user).
+#
+#      but then again - you should be bootstrapping into the root folder
+#      separately, see method 2:
+#
+# -- method 2: bootstrap root user
+#      run the one-liner like so:
+#        ALLOW_ROOT=1 ./scripts/bootstrap-local.sh 2>/dev/null
+#
+#      this will bootstrap even if the user is root.
+#      files will have to be managed separately for each user,
+#      but this method is considered more secure as two
+#      configurations don't overlap.
+
+# clear console screen
+clear
+
+# make sure we're within git repo
+if [ ! -r "$PWD/scripts/" ]; then
+  echo "fail:"
+  echo "  this script must run from the git repo"
+  echo "  type: cd .., then run:"
+  echo "    ./scripts/bootstrap-local.sh"
+  exit 1
+fi
+
+# bootstrap: not allowed for root user
+# unless: ALLOW_ROOT is set
+if [ "$ALLOW_ROOT" == "" ]; then
+  if [ "$USER" == "root" ]; then
+    echo "fail: root user not allowed."
+    exit 1
+  fi
+
+  if [ "$HOME" == "/root" ]; then
+    echo"fail: root user not allowed."
+    exit 1
+  fi
+fi
+
+# pushd "$HOME" >&/dev/null
+
+# fail: already bootstrapped
+# delete file to bootstrap again
+if [ -f "$HOME/.dotfiles_bootstrapped" ]; then
+  echo -e "fail:\n"
+  echo -e "  bootstrapped already.\n"
+  echo -e "  to bootstrap again run: rm -f $HOME/.dotfiles_bootstrapped\n"
+  echo -e "  then retry the bootstrap command."
+  # popd
+  exit 1
+fi
+
+echo "info: bootstrap in progress"
+
+# warn: files will be overwritten without prompt
+mkdir "$HOME/.local" >&/dev/null
+mkdir "$HOME/.local/bin" >&/dev/null
+
+cp "../.bash_aliases" "$HOME/.bash_aliases" >&/dev/null
+cp "../.bash_config" "$HOME/.bash_config" >&/dev/null
+cp "../.bash_exports" "$HOME/.bash_exports" >&/dev/null
+cp "../.bash_functions" "$HOME/.bash_functions" >&/dev/null
+# cp "../.bash_history" "$HOME/.bash_history" >&/dev/null
+cp "../.bash_logout" "$HOME/.bash_logout" >&/dev/null
+cp "../.bashrc" "$HOME/.bashrc" >&/dev/null
+cp "../.gitconfig" "$HOME/.gitconfig" >&/dev/null
+cp "../.nanorc" "$HOME/.nanorc" >&/dev/null
+cp "../.profile" "$HOME/.profile" >&/dev/null
+cp "../.wgetrc" "$HOME/.wgetrc" >&/dev/null
+
+touch "$HOME/.dotfiles_bootstrapped" >&/dev/null
+
+source .bashrc
+
+echo "pass: dotfiles bootstrapped"
+echo "note: re-open your terminal"
+
+# popd >&/dev/null
+exit 0
